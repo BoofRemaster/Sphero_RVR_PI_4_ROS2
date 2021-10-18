ada480ba46320aa155b334615797180b60d15dd3
Ubuntu <pi@RVR.modem>
2021-10-18 21:01:18 +1100

    drive_rvr.py works but needs optimising

--- rvr_pi_4/drive_rvr.py
+++ rvr_pi_4/drive_rvr.py
@@ -30,7 +30,7 @@ class Drive_Node(Node):
         # drive variables
         self.speed = 0 # 0-255
         self.heading = 0 # 0-359 {0 - Foward, 90 - Right, 180 - Back, 270 - Left}
-        self.flag = 0 # {0 - Normal, 1 - Reverse, 2 - Boost, 3 - Fast Turn}
+        self.flag = 0 # {0 - Drive, 1 - Reverse}
 
         loop.run_until_complete(self.drive_rvr())
 
@@ -55,45 +55,54 @@ class Drive_Node(Node):
     async def drive_rvr(self):
         while True:
             key_press = self.get_key()
+
+            if key_press == 'w':
+                if self.flag == 1:
+                    print('Drive.')
+                    self.speed = 0
+                else:
+                    print('Speeding Up Forward.')
+                    self.speed += 51
+                self.flag = 0
             
-            # Forward
-            if key_press == 'w' and self.flag == 0:
-                print('Moving Forward!')
-                self.speed = self.speed + 50
-            elif key_press == 'w' and self.speed > 0 and self.flag == 1:
-                # Change to drive
-                self.speed = 0
-                self.flag == 0
-            elif key_press == 'w' and self.flag == 1:
-                print('Slowing Reverse!')
-                self.speed = self.speed + 50
-            
-            # Reverse
-            if key_press == 's' and self.flag == 0:
-                print('Slowing Forward!')
-                self.speed = self.speed - 50
-                if self.speed < 0:
-                    self. speed = 0
-            elif key_press == 's' and self.speed > 0 and self.flag == 0:
-                # Change to Reverse
-                self.speed = 0
+            elif key_press == 's':
+                if self.flag == 0:
+                    print('Reverse.')
+                    self.speed = 0
+                else:
+                    print('Speeding Up Reverse.')
+                    self.speed += 51
                 self.flag = 1
-            elif key_press == 's' and self.flag == 1:
-                print('Reversing!')
-                self.speed = self.speed - 50
             
-            # Keep drive constraints
+            elif key_press == 'a':
+                print('Turning Left.')
+                self.heading -= 10
+            
+            elif key_press == 'd':
+                print('Turning Right.')
+                self.heading += 10
+            
+            elif key_press == 'space':
+                print('Stopping Drive Actions.')
+                self.speed = 0
+                self.flag = 0
+
+            # Exit the program
+            elif key_press == 'esc':
+                print('Exiting Drive Mode.')
+                print('You may have to type "reset" into terminal upon exiting.\nCurrent bug where terminal will not show what you are typing after the program stops.')
+                break
+            
+            # Manage drive constraints
             if self.speed > 255:
                 self.speed = 255
             elif self.speed < -255:
                 self.speed = -255
             
-            # Exit the program
-            if key_press == 'esc':
-                print('Exiting Drive Mode.')
-                print('''You may have to type 'reset' into terminal upon exiting.\n
-                Current bug where terminal will not show what you are typing after the program stops.''')
-                break
+            if self.heading > 359:
+                self.heading = self.heading - 359
+            elif self.heading < 0:
+                self.heading = 359 + self.heading
             
             await rvr.drive_with_heading(self.speed, self.heading, self.flag)
             await asyncio.sleep(0.1)
@@ -104,9 +113,8 @@ def main(args=None):
     #rclpy.spin(node)
     node.destroy_node()
     print('Node Destroyed.')
-    print('Shutting Down...')
+    print('Program Terminated')
     rclpy.shutdown()
 
 if __name__ == "__main__":
     main()
-    loop.close()
